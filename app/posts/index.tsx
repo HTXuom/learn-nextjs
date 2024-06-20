1c9d472a2e4a2164c9aeb1e78e605cab59fed5fd
HTXuom <xuom.ho@sapotacorp.vn>
2024-06-20 10:56:46 +0700

    commit file

--- app/posts/index.tsx
+++ app/posts/index.tsx
@@ -1,12 +1,18 @@
+import { link } from 'fs';
+import Link from 'next/link';
 import * as React from 'react';
 
 export interface PostListPageProps {
 }
 
+<Link href="/about">
+  <a> Go to about</a>
+</Link>
 export default function App(props: PostListPageProps) {
   return (
     <div>
           Post List Page 
     </div>
   );
+      
 }

0f2bdd189d27da9268c7f05f0ae9e8bbe4885814
HTXuom <xuom.ho@sapotacorp.vn>
2024-06-21 09:24:59 +0700

    commit file

--- app/posts/[postid].tsx
+++ app/posts/[postid].tsx
@@ -1,15 +1,52 @@
 import * as React from 'react';
-import {useRouter} from 'next/dist/client/router'
-export interface PostDetailPageProps {
+import { useRouter } from 'next/router';
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next';
+
+export interface PostPageProps {
+  post: { id: string; title: string };
 }
 
-export default function PostDetailPage(props: PostDetailPageProps) {
+export default function PostDetailPage(props: PostPageProps) {
+  const { post } = props;
+  const router = useRouter();
 
-    const router = useRouter()
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
   return (
-      <div>
-          <h1>Post List Page </h1>
-          <p>Query: {JSON.stringify(router.query)}</p>
+    <div>
+      <h1>Post Detail Page</h1>
+      <p>Query: {JSON.stringify(router.query)}</p>
+      <h2>{post.title}</h2>
     </div>
   );
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  console.log('Get static paths');
+  return {
+    paths: [
+      { params: { postId: '1' } },
+      { params: { postId: '2' } },
+      { params: { postId: '3' } },
+    ],
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps<PostPageProps> = async (
+  context: GetStaticPropsContext
+) => {
+  console.log('Get static Props', context.params?.postId);
+  const postId = context.params?.postId;
+  const response = await fetch(`https://api.example.com/posts/${postId}`); // Thay URL bằng URL thật của API
+  const data = await response.json();
+  console.log(data);
+
+  return {
+    props: {
+      post: { id: data.id, title: data.title },
+    },
+  };
+};
